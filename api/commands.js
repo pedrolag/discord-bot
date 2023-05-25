bd6f23466b66f38994259f8a35047bebedd4e2ed
Pedro Gonçalves <pedro.goncalves@paytrack.com.br>
2023-05-25 17:51:33 -0300

    Adapting the application to Vercel

--- api/commands.js
+++ api/commands.js
@@ -1,44 +1,5 @@
 import 'dotenv/config';
-import { getRPSChoices } from './game.js';
-import { capitalize, InstallGlobalCommands } from './utils.js';
-
-// Get the game choices from game.js
-function createCommandChoices() {
-  const choices = getRPSChoices();
-  const commandChoices = [];
-
-  for (let choice of choices) {
-    commandChoices.push({
-      name: capitalize(choice),
-      value: choice.toLowerCase(),
-    });
-  }
-
-  return commandChoices;
-}
-
-// Simple test command
-const TEST_COMMAND = {
-  name: 'test',
-  description: 'Basic command',
-  type: 1,
-};
-
-// Command containing options
-const CHALLENGE_COMMAND = {
-  name: 'challenge',
-  description: 'Challenge to a match of rock paper scissors',
-  options: [
-    {
-      type: 3,
-      name: 'object',
-      description: 'Pick your object',
-      required: true,
-      choices: createCommandChoices(),
-    },
-  ],
-  type: 1,
-};
+import { InstallGlobalCommands } from './utils.js';
 
 const CALL_FOR_VEHICLE_OWNER_COMMAND = {
   name: 'call',
@@ -55,8 +16,6 @@ const CALL_FOR_VEHICLE_OWNER_COMMAND = {
 };
 
 const ALL_COMMANDS = [
-  TEST_COMMAND,
-  CHALLENGE_COMMAND,
   CALL_FOR_VEHICLE_OWNER_COMMAND
 ];
 
